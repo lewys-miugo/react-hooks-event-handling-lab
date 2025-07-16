508ecda6d819176ed0be5835cdbfc77e7cbd5bdf
lewys-miugo <lewiswambugu01@gmail.com>
2025-07-16 15:24:40 +0300

    Handling functions

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe()
+{
+    function handleOnBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    function handleOnFocus(){
+        console.log("Good!")
+    }
+
+    return (
+        <button onBlur={handleOnBlur} onFocus={handleOnFocus}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
